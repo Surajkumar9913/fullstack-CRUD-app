8288e01a1a769aa238de032b9f9a4354af5042e2
Surajkumar9913 <surajyuva200@gmail.com>
2024-09-18 14:35:13 +0530

    proxy added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        proxy: {
-            "/api": "https://fullstack-crud-app-2vyb.onrender.com",
+      proxy: {
+        '/api': {
+          target: 'https://fullstack-crud-app-2vyb.onrender.com',
+          changeOrigin: true,
+          secure: false,
         },
+      },
     },
     plugins: [react()],
-});
+  });
+  
